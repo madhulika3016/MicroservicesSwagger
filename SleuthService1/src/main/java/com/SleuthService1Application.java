f646514753c1109dbce7f18112631d31d0aefc90
madhulika3016 <madhulikapa@gmail.com>
2024-05-02 22:39:30 +0530

    commit

---
+++ SleuthService1/src/main/java/com/SleuthService1Application.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SleuthService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthService1Application.class, args);
+	}
+}
+
+		
+	
+
+
